21696bb0717621f8168f2fc703bad6eed011edef
Zachary 2 <zteagar@msn.com>
2017-01-31 21:01:38 -0700

    Adding Files

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 var http = require('http');
 var fs = require('fs');
+var EventEmitter = require('events').EventEmitter;
+
 var port = 8080;
+var logger = new EventEmitter();
+
+logger.on('error', function(message) {
+    console.log('ERROR' + message);
+});
 http.createServer(function(request, response) {
 
     fs.readFile('index.html', function(err, content) {
@@ -8,6 +15,7 @@ http.createServer(function(request, response) {
             response.writeHead(404, {
                 'Content-Type': 'text/html'
             });
+            logger.emit('error', err);
             response.end('failed');
         } else {
             response.writeHead(200, {
