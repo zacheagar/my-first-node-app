8fd1a723fa679f5c60ee763135743df23933a708
Zachary 2 <zteagar@msn.com>
2017-01-31 20:32:50 -0700

    Added index.html file

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 var http = require('http');
+var fs = require('fs');
 var port = 8080;
 http.createServer(function(request, response) {
-    response.writeHead(200, {
-        'Content-Type': 'text/plain'
-    });
-    response.end("My first node app");
+
+    fs.readFile('index.html', function(err, content) {
+        if (err) {
+            response.writeHead(404, {
+                'Content-Type': 'text/html'
+            });
+            response.end('failed');
+        } else {
+            response.writeHead(200, {
+                'Content-Type': 'text/html'
+            });
+            response.end(content);
+        }
+    })
 }).listen(port);
 
 console.log('server running on port' + port);
